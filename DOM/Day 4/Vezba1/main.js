bba891ff5d6203f6fea19e4adf35f4ac007b50eb
BIT Student <classroom@bgit.rs>
2018-03-01 13:00:02 +0100

    Event

---
+++ DOM/Day 4/Vezba1/main.js
@@ -0,0 +1,12 @@
+function changeColor(){
+
+    // var ourDiv = document.querySelector("div");    
+    // if(ourDiv.classList.length == 0){
+    //     ourDiv.classList.add("container");
+    // } else {
+    //     ourDiv.classList.remove("container");
+    // }
+
+    var ourDiv = document.querySelector("div");
+    ourDiv.classList.toggle("container");
+}
---
+++ DOM/Day 4/Vezba2/main.js
@@ -0,0 +1,13 @@
+
+var mainDiv = document.querySelector(".ourDiv");
+var input = document.querySelector("input");
+var button = document.querySelector("button");
+
+button.onclick = function (){
+    var p = document.createElement("p");
+    var pText = document.createTextNode(input.value);
+    p.appendChild(pText);
+    mainDiv.appendChild(p);
+    input.value = "";
+}
+
